9ab2bc33405bed2ec75d36dae62b8a808fe2beb9
Dan Bishop <d@nbishop.uk>
2023-11-01 09:32:55 +0000

    Fixed

--- connect.sh
+++ connect.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # sudo check
-if [ "$EUID" -ne 0 ]
-  then echo "Please run with sudo"
-  exit
-fi
+# if [ "$EUID" -ne 0 ]
+#   then echo "Please run with sudo"
+#   exit
+# fi
 
 # Check there's only one wired connection
 number_connections=$(nmcli -t -f NAME,TYPE connection show --active|grep -ethernet|wc -l)
@@ -20,79 +20,56 @@ read -s password
 echo
 
 # Reset eduroam password
-cat >"/etc/NetworkManager/system-connections/eduroam.nmconnection" <<EOF
-[connection]
-id=eduroam
-uuid=aaed9a44-5d63-4701-bb13-bd71f913a798
-type=wifi
-interface-name=wlp1s0
+# cat >"/etc/NetworkManager/system-connections/eduroam.nmconnection" <<EOF
+# [connection]
+# id=eduroam
+# uuid=aaed9a44-5d63-4701-bb13-bd71f913a798
+# type=wifi
+# interface-name=wlp1s0
 
-[wifi]
-mode=infrastructure
-ssid=eduroam
+# [wifi]
+# mode=infrastructure
+# ssid=eduroam
 
-[wifi-security]
-auth-alg=open
-key-mgmt=wpa-eap
+# [wifi-security]
+# auth-alg=open
+# key-mgmt=wpa-eap
 
-[802-1x]
-anonymous-identity=@york.ac.uk
-ca-cert=/etc/ssl/certs/Comodo_AAA_Services_root.pem
-eap=ttls;
-identity=drb502@york.ac.uk
-password=$password
-phase2-auth=mschapv2
+# [802-1x]
+# anonymous-identity=@york.ac.uk
+# ca-cert=/etc/ssl/certs/Comodo_AAA_Services_root.pem
+# eap=ttls;
+# identity=drb502@york.ac.uk
+# password=$password
+# phase2-auth=mschapv2
 
-[ipv4]
-method=auto
+# [ipv4]
+# method=auto
 
-[ipv6]
-addr-gen-mode=stable-privacy
-method=auto
+# [ipv6]
+# addr-gen-mode=stable-privacy
+# method=auto
 
-[proxy]
-EOF
-chmod 600 "/etc/NetworkManager/system-connections/eduroam.nmconnection"
+# [proxy]
+# EOF
+# chmod 600 "/etc/NetworkManager/system-connections/eduroam.nmconnection"
 
 name=$(nmcli -t -f NAME,TYPE connection show --active|grep -ethernet|cut -d ":" -f1)
 uuid=$(nmcli -t -f UUID,TYPE connection show --active|grep -ethernet|cut -d ":" -f1)
 interfacename=$(nmcli -t -f DEVICE,TYPE connection show --active|grep -ethernet|cut -d ":" -f1)
 
-cat >"/etc/NetworkManager/system-connections/$name.nmconnection" <<EOF
-[connection]
-id=$name
-uuid=$uuid
-type=ethernet
-autoconnect-priority=-999
-interface-name=$interfacename
-timestamp=$EPOCHSECONDS
-
-[ethernet]
-
-[802-1x]
-anonymous-identity=@york.ac.uk
-ca-cert=/etc/ssl/certs/Comodo_AAA_Services_root.pem
-eap=peap;
-identity=drb502@york.ac.uk
-password=$password
-phase2-auth=mschapv2
-
-[ipv4]
-method=auto
-
-[ipv6]
-addr-gen-mode=stable-privacy
-method=auto
-
-[proxy]
-EOF
-
-chmod 600 "/etc/NetworkManager/system-connections/$name.nmconnection"
-
-nmcli connection reload
-
 echo "Disabling network connection..."
 nmcli connection down "$name"
+
 sleep 5
+
+echo "Applying network settings..."
+nmcli con modify "$name" 802-1x.eap peap\
+ 802-1x.identity drb502@york.ac.uk\
+ 802-1x.phase2-auth mschapv2\
+ 802-1x.password "$password"\
+ 802-1x.ca-cert "/etc/ssl/certs/Comodo_AAA_Services_root.pem"\
+ 802-1x.anonymous-identity "@york.ac.uk"
+
 echo "Re-enabling network connection..."
 nmcli connection up "$name"
